8d4c97e4be1eaf82ec92c5f427ee3c67be6c0691
Viishall01 <vishalvishwakarma11026@gmail.com>
2025-01-30 12:06:34 +0530

    applied fetching of users data and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react"
+import UserTile from "./components/UserTile";
 
 
 function App() {
-  
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  const fetchData = async () => {
+    try{
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      setUsers(data);
+    }catch(error){
+      console.log("Error in Fetching data", error);
+    }
+  };
 
   return (
-    <div className="text-red-600">
-    Hello World!
+    <div className="">
+      <div >
+        {users.map((user) => (
+          <UserTile key={user.id} id={user.id} name={user.name} email={user.email}/>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/components/UserTile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const UserTile = ({id, name, email}) => {
+  return (
+    <div className='flex mb-1 rounded-md max-w-96 justify-between items-center border border-black'>
+        <div className='flex flex-col m-2 '>
+            <h2><span className='p-2'>{id}</span>{name}</h2>
+            <h3 className='text-blue-600 text-sm mx-5'>{email}</h3>
+        </div>
+        <div className='flex m-2 items-center gap-2'> 
+            <button className=' px-2 cursor-pointer hover:scale-110 transition-all border border-gray-600 rounded-lg'>Edit</button>
+            <button className=' px-2 cursor-pointer hover:scale-110 transition-all border border-gray-600 rounded-lg'>Delete</button>
+        </div>
+    </div>
+  )
+}
+
+export default UserTile
